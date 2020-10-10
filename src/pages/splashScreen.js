ca45bb5cb1b203c8f371d970499cacae083a2f31
Joshua Draxten <joshuadraxten@live.com>
2020-10-10 18:25:34 -0500

    Fix how slash screen looks

--- src/pages/splashScreen.js
+++ src/pages/splashScreen.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+const splashScreenStyle = {
+    textAlign: "center",
+    position: "relative",
+    top: "50%",
+    transform: "translateY(-75%)"
+}
+const splashScreenH2 = {
+  fontSize: "48px",
+  margin: 0
+}
+
 export default () => {
-    return <div className="splash-screen">
+    return <div style={{height: "100vh"}}><div className="splash-screen" style={splashScreenStyle}>
         <img alt="" src="/images/icons-192.png"/>
-        <h2>Muah.bio</h2>
-    </div>
+        <h2 style={splashScreenH2}>Muah.bio</h2>
+    </div></div>
 }
