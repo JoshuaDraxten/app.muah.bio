4c4c427f445cd572df627c12ab3e3c0046b1519c
Joshua Draxten <joshuadraxten@live.com>
2020-10-15 16:09:37 -0500

    Endpoint for my own login system

---
+++ src/lambda/log-in.js
@@ -0,0 +1,50 @@
+const { SHA256 } = require("sha2");
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://joshuad:!7PrMT6ww&LqZDxgRU@cluster0.5j0rh.mongodb.net/user_data?retryWrites=true&w=majority";
+
+let cachedDb = null;
+function connectToDatabase (uri) {
+  console.log('=> connect to database');
+
+  if (cachedDb) {
+    console.log('=> using cached database instance');
+    return Promise.resolve(cachedDb);
+  }
+
+  return MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(db => {
+      cachedDb = db;
+      return cachedDb;
+    });
+}
+
+exports.handler = async event => {
+    const { usernameOrEmail, password } = event.queryStringParameters;
+    const client = await connectToDatabase(uri);
+    const collection = client.db("Muah_bio").collection("users");
+
+    const passwordEncrypted = SHA256(password).toString("base64"); 
+    console.log( passwordEncrypted )
+
+    let user;
+    if ( usernameOrEmail.match("@") ) {
+        const email = usernameOrEmail;
+        [ user ] = await collection.find( { email, password: passwordEncrypted } ).toArray();
+    } else {
+        const username = usernameOrEmail;
+        [ user ] = await collection.find( { "instagram.username": username, password: passwordEncrypted } ).toArray();
+    }
+
+    if ( !user ) {
+        return {
+            statusCode: 404,
+            body: JSON.stringify({error: "User Not Found"}) ,
+        }
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify( user ),
+    }
+
+}
