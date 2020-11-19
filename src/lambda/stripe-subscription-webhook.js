9f3e76eac8995a304e55d6c8b45a5098714aba2e
Joshua Draxten <joshuadraxten@live.com>
2020-11-18 18:28:10 -0600

    Update translations

--- src/lambda/stripe-subscription-webhook.js
+++ src/lambda/stripe-subscription-webhook.js
@@ -2,9 +2,9 @@
 // See your keys here: https://dashboard.stripe.com/account/apikeys
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-exports.handler = async ( event, context ) => {
-    const body = JSON.stringify( event.body );
-    const { headers } = event;
+exports.handler = async ( e ) => {
+    const body = JSON.stringify( e.body );
+    const { headers } = e;
 
     // Retrieve the event by verifying the signature using the raw body and secret.
     let event;
